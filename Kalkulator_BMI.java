ae9f09dfbb2a21fc08d9903e1c730684d9146a59
Vesse00 <vessepl@gmail.com>
2021-01-19 21:23:11 +0100

    nazwy

--- Kalkulator_BMI.java
+++ Kalkulator_BMI.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class trzecie {
+public class Kalkulator_BMI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
