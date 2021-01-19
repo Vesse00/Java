0fda813aecdfb505098147f0dc8d46180b8de6e5
Vesse00 <vessepl@gmail.com>
2021-01-19 21:24:42 +0100

    Update HASLO.java

--- HASLO.java
+++ HASLO.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-class pierwsze{
+class HASLO{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
             System.out.print("Podaj halo: ");
