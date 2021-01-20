77a02af2730e1089dc3c3c661aa83b0319693e8b
Vesse00 <vessepl@gmail.com>
2021-01-20 11:07:11 +0100

    Update figury.java

---
+++ 1.7/figury.java
@@ -0,0 +1,20 @@
+public class figury
+{
+    public static void main(String[] args) {
+        System.out.println("*******");
+        System.out.println("*     *");
+        System.out.println("*     *");
+        System.out.println("*******\n");
+
+        System.out.println("   * ");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******\n");
+
+        System.out.println("  ****  ");
+        System.out.println(" *    * ");
+        System.out.println(" *    * ");
+        System.out.println("  ****  ");
+        
+    }
+}
